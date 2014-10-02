4245a4ae7fbaeaad0c8bd2110a8fb55400b72856
Wiktor <iesekiel@gmail.com>
2014-10-02 23:37:24 +0300

    Created base hierarchy

---
+++ TaskFirstChristmasGift/TaskFirstChristmasGift/ChristmasGift.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFirstChristmasGift.Components;
+
+namespace TaskFirstChristmasGift
+{
+    public class ChristmasGift
+    {
+        private readonly ICollection<Component> _components = new List<Component>();
+
+        public void AddComponent(Component component)
+        {
+            _components.Add(component);
+        }
+    }
+}
---
+++ TaskFirstChristmasGift/TaskFirstChristmasGift/Components/Candy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFirstChristmasGift.Components
+{
+    public class Candy : Component
+    {
+        public override string ComponentDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

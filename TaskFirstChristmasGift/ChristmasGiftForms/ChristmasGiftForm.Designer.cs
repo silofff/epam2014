c2483561765ba90ab1d35fca13b5fd2c2920997b
Wiktor <iesekiel@gmail.com>
2014-10-12 22:53:34 +0300

    Changed Form GUI

--- TaskFirstChristmasGift/ChristmasGiftForms/ChristmasGiftForm.Designer.cs
+++ TaskFirstChristmasGift/ChristmasGiftForms/ChristmasGiftForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ChristmasGiftForms
+﻿using ChristmasGiftLibrary;
+
+namespace ChristmasGiftForms
 {
     partial class ChristmasGiftForm
     {
@@ -30,9 +32,21 @@
         {
             this.OpenButton = new System.Windows.Forms.Button();
             this.InfoTextBox = new System.Windows.Forms.RichTextBox();
-            this.addButton = new System.Windows.Forms.Button();
             this.saveGift = new System.Windows.Forms.Button();
             this.sortButton = new System.Windows.Forms.Button();
+            this.specialLabel = new System.Windows.Forms.Label();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.componentBox = new System.Windows.Forms.ComboBox();
+            this.addCandyButton = new System.Windows.Forms.Button();
+            this.sugarBox = new System.Windows.Forms.TextBox();
+            this.costBox = new System.Windows.Forms.TextBox();
+            this.weightBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.minBox = new System.Windows.Forms.TextBox();
+            this.maxBox = new System.Windows.Forms.TextBox();
+            this.findSugar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // OpenButton
@@ -51,21 +65,11 @@
             this.InfoTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.InfoTextBox.Location = new System.Drawing.Point(166, 14);
+            this.InfoTextBox.Location = new System.Drawing.Point(178, 14);
             this.InfoTextBox.Name = "InfoTextBox";
-            this.InfoTextBox.Size = new System.Drawing.Size(358, 301);
+            this.InfoTextBox.Size = new System.Drawing.Size(474, 491);
             this.InfoTextBox.TabIndex = 1;
             this.InfoTextBox.Text = "";
-            // 
-            // addButton
-            // 
-            this.addButton.Location = new System.Drawing.Point(12, 270);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(148, 43);
-            this.addButton.TabIndex = 2;
-            this.addButton.Text = "Add";
-            this.addButton.UseVisualStyleBackColor = true;
-            this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
             // saveGift
             // 
@@ -79,7 +83,7 @@
             // 
             // sortButton
             // 
-            this.sortButton.Location = new System.Drawing.Point(12, 219);
+            this.sortButton.Location = new System.Drawing.Point(12, 115);
             this.sortButton.Name = "sortButton";
             this.sortButton.Size = new System.Drawing.Size(148, 45);
             this.sortButton.TabIndex = 4;
@@ -87,20 +91,137 @@
             this.sortButton.UseVisualStyleBackColor = true;
             this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
             // 
+            // specialLabel
+            // 
+            this.specialLabel.AutoSize = true;
+            this.specialLabel.Location = new System.Drawing.Point(139, 402);
+            this.specialLabel.Name = "specialLabel";
+            this.specialLabel.Size = new System.Drawing.Size(0, 13);
+            this.specialLabel.TabIndex = 19;
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Location = new System.Drawing.Point(137, 376);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(28, 13);
+            this.costLabel.TabIndex = 18;
+            this.costLabel.Text = "Cost";
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Location = new System.Drawing.Point(137, 350);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(41, 13);
+            this.weightLabel.TabIndex = 17;
+            this.weightLabel.Text = "Weight";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(137, 324);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 16;
+            this.nameLabel.Text = "Name";
+            // 
+            // componentBox
+            // 
+            this.componentBox.FormattingEnabled = true;
+            this.componentBox.Location = new System.Drawing.Point(12, 437);
+            this.componentBox.Name = "componentBox";
+            this.componentBox.Size = new System.Drawing.Size(121, 21);
+            this.componentBox.TabIndex = 15;
+            this.componentBox.SelectedIndexChanged += new System.EventHandler(this.componentBox_SelectedIndexChanged);
+            // 
+            // addCandyButton
+            // 
+            this.addCandyButton.Location = new System.Drawing.Point(12, 464);
+            this.addCandyButton.Name = "addCandyButton";
+            this.addCandyButton.Size = new System.Drawing.Size(148, 41);
+            this.addCandyButton.TabIndex = 14;
+            this.addCandyButton.Text = "Add Component";
+            this.addCandyButton.UseVisualStyleBackColor = true;
+            this.addCandyButton.Click += new System.EventHandler(this.addCandyButton_Click);
+            // 
+            // sugarBox
+            // 
+            this.sugarBox.Location = new System.Drawing.Point(12, 399);
+            this.sugarBox.Name = "sugarBox";
+            this.sugarBox.Size = new System.Drawing.Size(121, 20);
+            this.sugarBox.TabIndex = 13;
+            // 
+            // costBox
+            // 
+            this.costBox.Location = new System.Drawing.Point(12, 373);
+            this.costBox.Name = "costBox";
+            this.costBox.Size = new System.Drawing.Size(121, 20);
+            this.costBox.TabIndex = 12;
+            // 
+            // weightBox
+            // 
+            this.weightBox.Location = new System.Drawing.Point(12, 347);
+            this.weightBox.Name = "weightBox";
+            this.weightBox.Size = new System.Drawing.Size(121, 20);
+            this.weightBox.TabIndex = 11;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(12, 321);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(121, 20);
+            this.nameBox.TabIndex = 10;
+            // 
+            // minBox
+            // 
+            this.minBox.Location = new System.Drawing.Point(12, 223);
+            this.minBox.Name = "minBox";
+            this.minBox.Size = new System.Drawing.Size(55, 20);
+            this.minBox.TabIndex = 20;
+            // 
+            // maxBox
+            // 
+            this.maxBox.Location = new System.Drawing.Point(85, 223);
+            this.maxBox.Name = "maxBox";
+            this.maxBox.Size = new System.Drawing.Size(54, 20);
+            this.maxBox.TabIndex = 21;
+            // 
+            // findSugar
+            // 
+            this.findSugar.Location = new System.Drawing.Point(12, 249);
+            this.findSugar.Name = "findSugar";
+            this.findSugar.Size = new System.Drawing.Size(148, 45);
+            this.findSugar.TabIndex = 22;
+            this.findSugar.Text = "Find By Sugar";
+            this.findSugar.UseVisualStyleBackColor = true;
+            this.findSugar.Click += new System.EventHandler(this.findSugar_Click);
+            // 
             // ChristmasGiftForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(536, 325);
+            this.ClientSize = new System.Drawing.Size(664, 515);
+            this.Controls.Add(this.findSugar);
+            this.Controls.Add(this.maxBox);
+            this.Controls.Add(this.minBox);
+            this.Controls.Add(this.specialLabel);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.componentBox);
+            this.Controls.Add(this.addCandyButton);
+            this.Controls.Add(this.sugarBox);
+            this.Controls.Add(this.costBox);
+            this.Controls.Add(this.weightBox);
+            this.Controls.Add(this.nameBox);
             this.Controls.Add(this.sortButton);
             this.Controls.Add(this.saveGift);
-            this.Controls.Add(this.addButton);
             this.Controls.Add(this.InfoTextBox);
             this.Controls.Add(this.OpenButton);
             this.Name = "ChristmasGiftForm";
             this.Text = "Chtistmas Gift";
-            this.Activated += new System.EventHandler(this.ChristmasGiftForm_Activated);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,9 +231,22 @@
 
         private System.Windows.Forms.Button OpenButton;
         private System.Windows.Forms.RichTextBox InfoTextBox;
-        private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button saveGift;
         private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Label specialLabel;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.ComboBox componentBox;
+        private System.Windows.Forms.Button addCandyButton;
+        private System.Windows.Forms.TextBox sugarBox;
+        private System.Windows.Forms.TextBox costBox;
+        private System.Windows.Forms.TextBox weightBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private readonly ChristmasGift _gift = new ChristmasGift();
+        private System.Windows.Forms.TextBox minBox;
+        private System.Windows.Forms.TextBox maxBox;
+        private System.Windows.Forms.Button findSugar;
     }
 }
 
--- TaskFirstChristmasGift/ChristmasGiftForms/ChristmasGiftForm.cs
+++ TaskFirstChristmasGift/ChristmasGiftForms/ChristmasGiftForm.cs
@@ -1,18 +1,20 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using ChristmasGiftForms.Properties;
 using ChristmasGiftLibrary;
+using ChristmasGiftLibrary.Components;
 
 namespace ChristmasGiftForms
 {
     public partial class ChristmasGiftForm : Form
     {
-        private ChristmasGift _gift = new ChristmasGift();
-        private readonly AddComponentForm _form = new AddComponentForm();
-
         public ChristmasGiftForm()
         {
             InitializeComponent();
+            componentBox.Items.Add("Candy");
+            componentBox.Items.Add("Fruit");
+            componentBox.SelectedItem = "Candy";
         }
 
         private void OpenButton_Click(object sender, EventArgs e)
@@ -60,12 +62,6 @@ namespace ChristmasGiftForms
             InfoTextBox.Text += "Total Sugar: " + gift.TotalSugar;
         }
 
-        private void addButton_Click(object sender, EventArgs e)
-        {
-            _form.GiftInitialize(ref _gift);
-            _form.Show(this);
-        }
-
         private void saveGift_Click(object sender, EventArgs e)
         {
             var saveFileDialog = new SaveFileDialog {FilterIndex = 1, RestoreDirectory = true};
@@ -86,15 +82,68 @@ namespace ChristmasGiftForms
             }
         }
 
-        private void ChristmasGiftForm_Activated(object sender, EventArgs e)
-        {
-            WriteInfo(_gift);
-        }
-
         private void sortButton_Click(object sender, EventArgs e)
         {
             _gift.SortGiftByWeight();
             WriteInfo(_gift);
+        }
+
+        private void componentBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch ((string)componentBox.SelectedItem)
+            {
+                case "Candy":
+                    specialLabel.Text = "Sugar";
+                    break;
+                case "Fruit":
+                    specialLabel.Text = "Color";
+                    break;
+            }
+        }
+
+        private void addCandyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch ((string) componentBox.SelectedItem)
+                {
+                    case "Candy":
+                        _gift.Add(new Candy(nameBox.Text, Double.Parse(weightBox.Text),
+                            Int32.Parse(sugarBox.Text), Double.Parse(costBox.Text)));
+                        break;
+
+                    case "Fruit":
+                        _gift.Add(new Fruit(nameBox.Text, Double.Parse(weightBox.Text),
+                            sugarBox.Text, Double.Parse(costBox.Text)));
+                        break;
+                }
+                WriteInfo(_gift);
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Can't added");
+            }
+        }
+
+        private void findSugar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sugars = _gift.FindComponentsBySugar(int.Parse(minBox.Text), int.Parse(maxBox.Text));
+
+                InfoTextBox.Text += "\nComponents with sugar: \n";
+                foreach (var sugar in sugars)
+                {
+                    InfoTextBox.Text += sugar.ComponentDescription() + "\n";
+                }
+                
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't find");
+            }
         }
     }
 }

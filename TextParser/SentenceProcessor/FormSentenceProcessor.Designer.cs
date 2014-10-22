43cdef85d59ab6ab27b6ca6c4fbf85e2324c808e
Wiktor <iesekiel@gmail.com>
2014-10-22 21:48:42 +0300

    Add new project Sentence Processor and add fileOpen to it.

---
+++ TextParser/SentenceProcessor/FormSentenceProcessor.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SentenceProcessor
+{
+    partial class FormSentenceProcessor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // FormSentenceProcessor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 385);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "FormSentenceProcessor";
+            this.Text = "Sentence Processor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+    }
+}
+
---
+++ TextParser/SentenceProcessor/FormSentenceProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TextParser;
+
+namespace SentenceProcessor
+{
+    public partial class FormSentenceProcessor : Form
+    {
+        public FormSentenceProcessor()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            try
+            {
+                var fileName = openFileDialog.FileName;
+                if (fileName == null) return;
+
+                var fileOpen = new FileOpener();
+                fileOpen.OpenFile(fileName);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't open file");
+            }
+        }
+    }
+}
